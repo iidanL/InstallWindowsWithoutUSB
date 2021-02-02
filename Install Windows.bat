95ec9e5914ed6932c33d400a16feabae05bbad92
itzdanL <52887424+itzdanL@users.noreply.github.com>
2021-02-02 02:09:10 +0200

    First Commit

---
+++ Install Windows.bat
@@ -0,0 +1,28 @@
+@echo off
+title Install Windows without USB
+
+
+echo Enter full path for iso.
+set /p a=
+	goto :extract
+	
+	
+:extract
+cls
+
+echo %a%
+
+PowerShell Mount-DiskImage -ImagePath "%a%"
+echo New Drive appeared on your pc, which drive letter it have? (Drive named with your iso name. example - A).
+set /p s=
+	
+	echo Type Drive Letter for Windows which you created before (You can't select your partition with already installed Windows).
+	set /p d=
+		DISM /Apply-Image /ImageFile:%s%:\sources\install.wim /Index:1 /ApplyDir:%d%:\
+		bcdboot %d%:\Windows
+	
+		PowerShell Dismount-DiskImage -ImagePath "%a%"
+		
+	echo Done. Reboot your pc to access your new Windows.
+pause
+cls
