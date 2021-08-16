a053f341ff40c951f5a774d9b0d5a9306cd8c755
Daniil <52887424+iidanL@users.noreply.github.com>
2021-08-16 03:22:48 +0300

    Add files via upload

--- Install Windows.bat
+++ Install Windows.bat
@@ -1,29 +1,45 @@
+<# : Installation.bat
 @echo off
+setlocal
 title Install Windows without USB
 
+:: CHECK FOR ADMIN PRIVILEGES
+dism >nul 2>&1 || (echo This script must be Run as Administrator. && pause && exit /b 1)
 
-echo Enter full path for iso.
-set /p a=
-	goto :extract
-	
-	
-:extract
-cls
 
-echo %a%
+echo Choose ISO to install:
 
-PowerShell Mount-DiskImage -ImagePath "%a%"
-echo New Drive appeared on your pc, which drive letter it have? (Drive named with your iso name. example - A).
-set /p s=
-	
+for /f "delims=" %%I in ('powershell -noprofile "iex (${%~f0} | out-string)"') do (
+	echo %%~I
+    7z.exe x -y -oC:\WindowsInstallation %%~I 
+)
+	cls
 	echo Type Drive Letter for Windows which you created before (You can't select your partition with already installed Windows).
 	set /p d=
-		IF EXIST "%s%:\sources\install.wim" DISM /Apply-Image /ImageFile:%s%:\sources\install.wim /Index:1 /ApplyDir:%d%:\
-		IF EXIST "%s%:\sources\install.esd" DISM /Apply-Image /ImageFile:%s%:\sources\install.esd /Index:1 /ApplyDir:%d%:\
-		bcdboot %d%:\Windows
 	
-		PowerShell Dismount-DiskImage -ImagePath "%a%"
-		
+		IF EXIST "C:\WindowsInstallation\autounattend.xml" (
+			IF EXIST "C:\WindowsInstallation\sources\install.wim" DISM /Apply-Image /ImageFile:C:\WindowsInstallation\sources\install.wim /Apply-Unattend:C:\WindowsInstallation\autounattend.xml /Index:1 /ApplyDir:%d%:\
+			IF EXIST "C:\WindowsInstallation\sources\install.esd" DISM /Apply-Image /ImageFile:C:\WindowsInstallation\sources\install.esd /Apply-Unattend:C:\WindowsInstallation\autounattend.xml /Index:1 /ApplyDir:%d%:\
+
+		) ELSE (
+			IF EXIST "C:\WindowsInstallation\sources\install.wim" DISM /Apply-Image /ImageFile:C:\WindowsInstallation\sources\install.wim /Index:1 /ApplyDir:%d%:\
+			IF EXIST "C:\WindowsInstallation\sources\install.esd" DISM /Apply-Image /ImageFile:C:\WindowsInstallation\sources\install.esd /Index:1 /ApplyDir:%d%:\
+		)
+		move C:\WindowsInstallation\sources\$OEM$\$$\Setup\Scripts\*.* %d%:\Windows\Setup\Scripts >nul 2>&1
+		move C:\WindowsInstallation\autounattend.xml %d%:\Windows\System32\Sysprep\unattend.xml >nul 2>&1
+		bcdboot %d%:\Windows
+	cls
 	echo Done. Reboot your pc to access your new Windows.
-pause
-cls
+	pause
+	
+goto :EOF
+: end Batch portion / begin PowerShell hybrid chimera #>
+
+Add-Type -AssemblyName System.Windows.Forms
+$f = new-object Windows.Forms.OpenFileDialog
+$f.InitialDirectory = pwd
+$f.Filter = "Image Files (*.iso)|*.iso|All Files (*.*)|*.*"
+$f.ShowHelp = $true
+$f.Multiselect = $true
+[void]$f.ShowDialog()
+if ($f.Multiselect) { $f.FileNames } else { $f.FileName }
