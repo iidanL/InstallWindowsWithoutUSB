b811bbff88e51e5f69625e27c272bda2a61a3b11
Zusier <zusier@disroot.org>
2021-10-16 13:39:12 -0500

    cd to %~dp0

--- Install Windows.bat
+++ Install Windows.bat
@@ -2,11 +2,12 @@
 @echo off
 setlocal
 title Install Windows without USB
+cd %~dp0
 
 :: CHECK FOR ADMIN PRIVILEGES
 dism >nul 2>&1 || (echo This script must be Run as Administrator. && pause && exit /b 1)
 
-if not exist %~dp07z.exe echo 7Zip not found! Please download this repo as a .zip and try again. & pause & exit
+if not exist 7z.exe echo 7Zip not found! Please download this repo as a .zip and try again. & pause & exit
 
 echo Choose ISO to install:
 
